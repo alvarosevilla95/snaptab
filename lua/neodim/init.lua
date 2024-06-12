ae326c4e27c32cefc797196e2d5aa0d51db72f07
Alvaro <alvaro@chainalysis.com>
2024-06-13 00:57:33 +0200

    Telescope picker

--- lua/neodim/init.lua
+++ lua/neodim/init.lua
@@ -179,3 +179,64 @@ local function rename_layout(name)
     end
   end
 end
+
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local sorters = require("telescope.sorters")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+local function snapshots_picker()
+  pickers
+      .new({}, {
+        initial_mode = "normal",
+        prompt_title = "Select a Snapshot",
+        finder = finders.new_table({
+          results = snapshots,
+          entry_maker = function(entry)
+            return {
+              value = entry,
+              display = entry.name,
+              ordinal = entry.name,
+              name = entry.name,
+            }
+          end,
+        }),
+        sorter = sorters.get_generic_fuzzy_sorter(),
+        attach_mappings = function(_, map)
+          local function enter_handler(prompt_bufnr)
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            if selection then
+              print(selection.name)
+              open_layout(selection.name)
+            end
+          end
+
+          local function delete_handler(prompt_bufnr)
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            if selection then
+              delete_layout(selection.name)
+            end
+            snapshots_picker()
+          end
+
+          local function rename_handler(prompt_bufnr)
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            if selection then
+              rename_layout(selection.name)
+            end
+            snapshots_picker()
+          end
+
+          map("i", "<CR>", enter_handler)
+          map("n", "<CR>", enter_handler)
+          map("n", "dd", delete_handler)
+          map("n", "r", rename_handler)
+          return true
+        end,
+      })
+      :find()
+end
