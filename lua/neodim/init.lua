fc7d695ed18d4449c8ee52fea4990ef9a5bfa652
Alvaro <alvaro@chainalysis.com>
2024-06-14 12:22:49 +0200

    Update keymaps

--- lua/neodim/init.lua
+++ lua/neodim/init.lua
@@ -4,11 +4,13 @@ local prev_layout = require("neodim.management").prev_layout
 local shift_layout_front = require("neodim.management").shift_layout_front
 local shift_layout_back = require("neodim.management").shift_layout_back
 local snapshots_picker = require("neodim.telescope").snapshots_picker
+local delete_buffers_not_in_layout = require("neodim.management").delete_buffers_not_in_layout
 
 vim.keymap.set("n", "<leader>tt", snapshots_picker, { silent = true })
 vim.keymap.set("n", "<leader>T", new_layout, { silent = true })
-vim.keymap.set("n", "<leader>tn", next_layout, { silent = true })
-vim.keymap.set("n", "<leader>tN", shift_layout_front, { silent = true })
-vim.keymap.set("n", "<leader>tp", prev_layout, { silent = true })
-vim.keymap.set("n", "<leader>tP", shift_layout_back, { silent = true })
+vim.keymap.set("n", "<leader>tj", next_layout, { silent = true })
+vim.keymap.set("n", "<leader>tJ", shift_layout_front, { silent = true })
+vim.keymap.set("n", "<leader>tk", prev_layout, { silent = true })
+vim.keymap.set("n", "<leader>tK", shift_layout_back, { silent = true })
 vim.keymap.set("n", "<leader>tr", snapshots_picker, { silent = true })
+vim.keymap.set("n", "<leader>dh", delete_buffers_not_in_layout, { silent = true })
